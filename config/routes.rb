1e0ceebf85e58664d52aa10434f55f53485f6337
tatyramirezgmj <tatiana@trwdesignsolutions.com>
2017-08-08 23:50:08 -0400

    update in routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resource :users
   resources :favorite_cities
   root 'users#index'
+  
+  get '/', to: redirect('/login')
 
   get 'users/new', as: 'signup'
   get '/login'  => 'sessions#new', as: "login"
